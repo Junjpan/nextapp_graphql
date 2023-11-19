5967caaee097c866dc2f03a54806aa3f18a9e325
JunjPan <elisepjj@hotmail.com>
2023-11-19 14:05:31 -0800

    HOC with forwarded ref

--- components/HOC.tsx
+++ components/HOC.tsx
@@ -1,36 +1,37 @@
-import React, { ReactNode, forwardRef, ComponentType, createRef } from "react";
+import React, { ReactNode, forwardRef, ComponentType } from "react";
 
 interface Props {
   children?: ReactNode;
   type: "submit" | "button";
-  onClick: () => void;
+  onClick?: () => void;
+  onHover?: () => void;
 }
 
 const Button = forwardRef<HTMLButtonElement, Props>((props, ref) => {
   return (
-    <button ref={ref} onClick={props.onClick}>
+    <button
+      ref={ref}
+      onClick={props.onClick}
+      style={{ height: "25px" }}
+      onMouseOver={props.onHover}
+    >
       {props.children}
     </button>
   );
 });
 
 const HOC = <P extends Object>(Component: ComponentType<P>) => {
-  const componentRef = createRef();
-
   const clickButton = () => {
     console.log("click");
-    console.log(
-      (componentRef.current as HTMLBodyElement).getBoundingClientRect().height
-    );
   };
 
-  const newComponent = (props: P) => {
-    return <Component ref={componentRef} onClick={clickButton} {...props} />;
-  };
+  const newComponent = forwardRef<HTMLButtonElement, P>((props, ref) => {
+    return <Component ref={ref} onClick={clickButton} {...props} />;
+  });
 
   return newComponent;
 };
 
 const HOCButton3 = HOC(Button);
 
-export default HOCButton3;
+export { HOCButton3, Button };
--- pages/test/hoc/index.tsx
+++ pages/test/hoc/index.tsx
@@ -1,11 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useEffect, createRef, RefObject } from "react";
 import {
   HOCButton1,
   HOCButton2,
 } from "../../../components/HigherOrderComponent";
-import HOCButton3 from "../../../components/HOC";
+import { HOCButton3, Button } from "../../../components/HOC";
 
 const Output = () => {
+  const buttonRef = createRef() as RefObject<HTMLButtonElement>;
+  const hocRef = createRef() as RefObject<HTMLButtonElement>;
+
+  const getHeight = () => {
+    console.log(buttonRef.current?.getBoundingClientRect().height);
+  };
+
+  const getWidth = () => {
+    console.log(hocRef.current?.getBoundingClientRect().width);
+  };
   useEffect(() => {
     if (HOCButton2.add) {
       console.log(HOCButton2.add(2, 4));
@@ -16,7 +26,12 @@ const Output = () => {
     <div>
       <HOCButton1 name="HOC Button1" />
       <HOCButton2 />
-      <HOCButton3>HOC with Ref Button</HOCButton3>
+      <Button type="button" ref={buttonRef} onClick={getHeight}>
+        Button 1
+      </Button>
+      <HOCButton3 ref={hocRef} type={"submit"} onHover={getWidth}>
+        HOC with Ref Button
+      </HOCButton3>
     </div>
   );
 };
